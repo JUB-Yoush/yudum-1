7b5e6393aa8546fad6d1c9204d8f7d2f4596147d
JUB_Yoush <yoush1337@gmail.com>
2022-05-12 14:53:03 -0400

    added killing mobs, add turn system

--- src/Levels/Main.gd
+++ src/Levels/Main.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-#onready var player = get_node("Player")
+onready var player = get_node("Player")
 func _ready() -> void:
 	pass
-	#player.connect("player_turn_ended",self,"on_player_turn_ended")
+	player.connect("turn_ended",self,"on_player_turn_ended")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,10 +11,6 @@ func _ready() -> void:
 #	pass
 
 func on_player_turn_ended():
-	
-	#loop thru every enemy
-	#have them run their action
-	#once all have run, run player.start_turn()
 	
 	var mobs := get_tree().get_nodes_in_group("mobs")
 	for mob in mobs:
